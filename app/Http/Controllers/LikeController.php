b07214e90a42e5f7db030a70ea447c34d7a43e71
smaiifakher <smaiiweldkasserine@gmail.com>
2021-09-01 15:29:36 +0100

    add:migration,controller,model

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Like;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class LikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Like $like
+     * @return void
+     */
+    public function show(Like $like)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Like $like
+     * @return Response
+     */
+    public function edit(Like $like)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Like $like
+     * @return Response
+     */
+    public function update(Request $request, Like $like)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Like $like
+     * @return Response
+     */
+    public function destroy(Like $like)
+    {
+        //
+    }
+}
