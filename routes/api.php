e85c86bf017085ef4f817a5d6309f39918975b01
smaiifakher <smaiiweldkasserine@gmail.com>
2021-09-09 20:58:57 +0100

    JWT package

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\QuestionController;
@@ -21,9 +22,21 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/question', QuestionController::class);
 Route::apiResource('/category', CategoryController::class);
 Route::apiResource('/question/{question}/reply', ReplyController::class);
-Route::post('/like/{reply}',[LikeController::class,'LikeIt']);
-Route::delete('/like/{reply}',[LikeController::class,'unLikeIt']);
+Route::post('/like/{reply}', [LikeController::class, 'LikeIt']);
+Route::delete('/like/{reply}', [LikeController::class, 'unLikeIt']);
 
+Route::group([
 
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
 
 
